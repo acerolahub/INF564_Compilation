141fb58921f033fe75930ced307b39f56c21ceef
Ouatt-Isma <54633761+Ouatt-Isma@users.noreply.github.com>
2021-02-13 20:54:28 +0100

    Update Main.java

--- src/mini_c/Main.java
+++ src/mini_c/Main.java
@@ -38,6 +38,42 @@ public class Main {
             Typing typer = new Typing();
             typer.visit(f);
             File tf = typer.getFile();
+            
+            
+            
+            
+            System.out.println(tf.funs.size());
+            for(Decl_fun d: tf.funs) {
+            	System.out.println("fun_type: "+d.fun_typ); 
+            	System.out.println("fun_name: "+d.fun_name); 
+            	for (Decl_var dd: d.fun_formals){
+            		System.out.println("var_type, var_name: "+dd.t + ", "+dd.name); 
+            	}
+            	if(d.fun_body instanceof Sblock) {
+            		Sblock tmp = (Sblock)d.fun_body; 
+            		System.out.println("Deb Sblock"); 
+            		for (Decl_var dd: tmp.dl){
+                		System.out.println("var_type, var_name: "+dd.t + ", "+dd.name); 
+                	}
+            		for(Stmt sl: tmp.sl) {
+            			if(sl instanceof Sreturn) {
+            				System.out.println("Sreturn : "+((Sreturn)sl).e); 
+            			}
+            			else {
+            				System.out.println("sl not instance of Sretrun"); 
+            			}
+            		}
+            		System.out.println("Fin Sblock"); 
+            	}
+            	else {
+            		System.out.println("fun_body not instance of sblock"); 
+            	}
+            	System.out.println(); 
+            }
+            
+            
+            
+            
             if (type_only) System.exit(0);
         } catch (Error e) {
         	System.out.println("error: " + e.getMessage());
