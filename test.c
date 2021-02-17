5b0f129e4d2b2ed3512fa24fd777b0f73f31ddf1
Ouatt-Isma <54633761+Ouatt-Isma@users.noreply.github.com>
2021-02-17 19:24:18 +0100

    Update test.c

--- test.c
+++ test.c
@@ -4,6 +4,83 @@
 // la commande 'make' recompile mini-c (si nécessaire)
 // et le lance sur ce fichier
 
+
+/*
 int main() {
-  return 0;
+	int moi; 
+	int toi; 
+	//return a + 2 + 5;
+    return -2 + a * 2;
 }
+*/
+
+/*
+int main(){
+	
+	int toi;
+	
+	return 1; 
+	
+	if(1){
+		int a; 
+		int b;
+	}
+	else{
+		;	
+	}
+	
+	return moi;
+}
+*/
+int main(){
+	
+	if(1){
+		int a; 
+		int b;
+	}
+	else{
+		;	
+	}
+	return moi;
+}
+
+/*x
+int main(){
+	int toi;
+	
+	return moi; 
+	
+	if(1){
+		int a; 
+	}
+	else{
+		;	
+	}
+	
+	return moi;
+}
+*/
+
+/*
+int main2() {
+	int moi; 
+	int toi; 
+  return 3 - 1 +3*4;
+}
+*/
+/*
+int main2() {
+	int moi; 
+	int toi; 
+  return a + 2 ;
+}
+*/
+
+
+/*
+int main() {
+	int moi; 
+	int toi; 
+  return -(moi + 2 + toi);
+}
+*/
